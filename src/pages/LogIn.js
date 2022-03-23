f57eacf5f1e7ac813e4c01a47c6a1688e9c9196e
Cat <cathy.moran@teladochealth.com>
2022-03-22 19:29:39 -0500

    login page created with register and login capabilities

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const LogIn = props => {
 	const [token, setToken] = useState('');
@@ -10,6 +11,13 @@ const LogIn = props => {
 	const [toggle, setToggle] = useState(false);
 	const [showForm, setShowForm] = useState(true);
 
+	useEffect(() => {
+		if (window.localStorage.getItem('token')) {
+			setToken(window.localStorage.getItem('token'));
+			setLoggedInUser(window.localStorage.getItem('loggedInUser'));
+		}
+	}, []);
+
 	const handleChange = e => {
 		setUser({ ...user, [e.target.id]: e.target.value });
 	};
@@ -17,32 +25,22 @@ const LogIn = props => {
 	const handleLogin = async e => {
 		e.preventDefault();
 		try {
-			const response = await fetch(`http://localhost:3000/users`, {
-				// credentials: 'include',
+			const response = await fetch(`http://localhost:3000/login`, {
 				method: 'POST',
-				// mode: 'no-cors',
 				headers: {
 					'Content-Type': 'application/json'
 				},
 				body: JSON.stringify(user)
 			});
-			console.log("user: ", user)
-			console.log("response: ", response)
 			const data = await response.json();
-			
-			console.log("data: ", data)
-
 			setToken(data.token);
 			setLoggedInUser(data.user.username);
 			window.localStorage.setItem('token', data.token);
 			window.localStorage.setItem('loggedInUser', data.user.username);
 		} catch (error) {
 			console.error(error);
-			alert('Username / password invalid');
+			alert('Username / password invalid');		
 		} 
-		// finally {
-		// 	window.location.assign('/login');
-		// }
 	};
 
 	const handleRegister = async e => {
@@ -66,10 +64,7 @@ const LogIn = props => {
 		} catch (error) {
 			console.error(error);
 			alert('Username already taken');
-		}
-		// } finally {
-		// 	window.location.assign('/login');
-		// }
+		} 
 	};
 
 	const displayForm = () => {
@@ -78,7 +73,6 @@ const LogIn = props => {
 
 	const logout = () => {
 		window.localStorage.clear();
-		window.localStorage.assign('/');
 	};
 
 	return (
@@ -124,9 +118,11 @@ const LogIn = props => {
 						) : (
 							<>
 								<div>Hello, {loggedInUser}! You are logged in.</div>
-								<button className="logOutBut btn btn-danger" onClick={logout}>
-									Log Out
-								</button>
+								<Link to="/">
+									<button className="logOutBut btn btn-danger" onClick={logout}>
+										Log Out
+									</button>
+								</Link>
 							</>
 						)}
 					</>
@@ -162,86 +158,3 @@ const LogIn = props => {
 };
 
 export default LogIn;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import { useParams } from 'react-router-dom';
-// import {useAppState} from '../AppState.jsx';
-
-// const Auth = (props) => {
-//     const type = useParams().form;
-//     const [formData, setFormData] = React.useState({
-//         username: "",
-//         password: ""
-//     });
-//     const [userData, setUserData] = React.useState(null);
-//     const {state, dispatch} = useAppState();
-
-//     React.useEffect(() => {
-//         if (userData) {
-//             console.log(userData)
-//             const {token, user} = userData;
-//             dispatch ({type: "auth", payload: {token, username: user.username}})
-//         }
-//     }, [userData]);
-
-//     const actions = {
-//         signup: () => {
-//             return fetch(state.url + "/users", {
-//                 method: "post",
-//                 headers: {
-//                     "Content-Type": "application/json",
-//                 },
-//                 body: JSON.stringify(formData),
-//             }).then((response) => response.json());
-//         },
-//         login: () => {
-//             return fetch(state.url + "/login", {
-//                 method: "post",
-//                 headers: {
-//                     "Content-Type": "application/json",
-//                 },
-//                 body: JSON.stringify(formData),
-//             }).then((response) => response.json());
-//         },
-//     }
-
-//     const handleChange = (e) => {
-//         setFormData({...formData, [e.target.name] : e.target.value})
-//     };
-
-//     const handleSubmit = (e) => {
-//         e.preventDefault();
-//         actions[type]().then((data) => {
-//             setUserData(data)
-//         })
-//     };
-    
-//     return (
-//         <>
-//             <h1>{type}</h1>
-//             <div>
-//                 <form onSubmit={handleSubmit}> 
-//                     <input type="text" name="username" value={formData.username} onChange={handleChange} />
-//                     <input type="password" name="password" value={formData.password} onChange={handleChange}/>
-//                     <input type="submit" value={type} />
-//                 </form>
-//             </div>
-//         </>
-//     )
-// };
-
-// export default Auth;
