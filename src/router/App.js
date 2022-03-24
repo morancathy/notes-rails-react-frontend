949c1cd35afc8a4be244b9f05ba2d620ca4b97a4
Cat <cathy.moran@teladochealth.com>
2022-03-23 22:55:39 -0500

    add route for carddetails page

--- src/router/App.js
+++ src/router/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Nav from '../components/Nav'
 import Home from '../pages/Home'
 import LogIn from '../pages/LogIn'
+import CardDetails from '../pages/CardDetails'
 
 export const App = (props) => {
   return (
@@ -12,6 +13,7 @@ export const App = (props) => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LogIn />} />
+        <Route path="/carddetails" element={<CardDetails />} />
       </Routes>
     </Router>
   </>);
