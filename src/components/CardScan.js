945dcdf8580b830c879b25b66b7e973cda49fc99
Alexander Franco <alexfranco12@gmail.com>
2022-03-24 13:16:50 -0500

    added upload document functionality

--- src/components/CardScan.js
+++ src/components/CardScan.js
@@ -1,10 +1,13 @@
 import React, {useState} from "react";
 import { useNavigate } from "react-router-dom";
-import {CardScanView} from "@cardscan.ai/insurance-cardscan-react";
+import { CardScanView, CardScanApi } from "@cardscan.ai/insurance-cardscan-react";
 
 export default function CardScan() {
   const [sessionToken, setSessionToken] = useState('');
   const [showScan, setShowScan] = useState(true);
+  const [selectedFile, setSelectedFile] = useState();
+  const [isSelected, setIsSelected] = useState(false)
+
   let navigate = useNavigate();
 
   // pulls data from scanned card
@@ -43,12 +46,35 @@ export default function CardScan() {
 			})
       .then((res) => res.json())
       .then((data) => {
+        console.log(data)
         setSessionToken(data.token.Token);
       })
       setShowScan(true)
 		} catch (error) {
 			console.error(error);
 		} 
+	};
+
+  const changeHandler = (event) => {
+		setSelectedFile(event.target.files[0]);
+		setIsSelected(true);
+	};
+
+	const handleSubmission = (e) => {
+    e.preventDefault();
+    const client = new CardScanApi({
+      "sessionToken": "eyJraWQiOiJ1cy1lYXN0LTExIiwidHlwIjoiSldTIiwiYWxnIjoiUlM1MTIifQ.eyJzdWIiOiJ1cy1lYXN0LTE6Mjg2NDkzNTYtYzQyMC00YjA2LTlhN2EtNjA0Y2E1MWJiZWQ0IiwiYXVkIjoidXMtZWFzdC0xOjg1ZGY0MWZkLTU5MGItNDIxYS04NTdiLTlmMTBjNDUzMmViOCIsImFtciI6WyJhdXRoZW50aWNhdGVkIiwiY2FyZHNjYW4tc2FuZGJveC1jbGllbnQtdXMtZWFzdC0xIiwiY2FyZHNjYW4tc2FuZGJveC1jbGllbnQtdXMtZWFzdC0xOnVzLWVhc3QtMTo4NWRmNDFmZC01OTBiLTQyMWEtODU3Yi05ZjEwYzQ1MzJlYjg6M2QzMTQyM2YtYWI1MC00ZDIwLTk1ZmItOWUyYzc5YzZkNTA0fDA5ODJiYTdhLTk2ZGUtNGRlMy1hNWE4LWViYzI1ODQ1YTExYSJdLCJodHRwczovL2F3cy5hbWF6b24uY29tL3RhZ3MiOnsicHJpbmNpcGFsX3RhZ3MiOnsiYWNjb3VudF9pZCI6WyIzZDMxNDIzZi1hYjUwLTRkMjAtOTVmYi05ZTJjNzljNmQ1MDQiXSwidHlwZSI6WyJkZXZlbG9wZXJfaWRlbnRpdHkiXSwiZGV2ZWxvcGVyX3VzZXJfaWQiOlsiMDk4MmJhN2EtOTZkZS00ZGUzLWE1YTgtZWJjMjU4NDVhMTFhIl19fSwiaXNzIjoiaHR0cHM6Ly9jb2duaXRvLWlkZW50aXR5LmFtYXpvbmF3cy5jb20iLCJleHAiOjE2NDgxNDg0MzksImlhdCI6MTY0ODE0NDgzOX0.OYQTDUq69MPUYk3ICZnYQPxsG69qS5iMVDW1CJibiT_4nfRvHmad_U83Kvvqqumx_xWRDH8O2C7TBkZ6XBweOUhC0K6OD7JfTnAlcD8CD6gXXw4R5GDhroZ9EKRcmfxm4jBfQFZf0mDRmGQxL4kJ9jXH9D7q_SuGhpH3ZHNjq0e-9dgTmhWd_XwQiCleNI0Vts4IqvVThct9tCtBkbja7GadMPLPXKJ4hv2lGR0kbttvGTMRQD7Xc16hC4k6FM8hdxMo1ToGs-EMXSL9siYHXcjR2_akwYQ3S8tKlMjffekrzPADK59N-Cg9adLiZV0EL7GjcFLo3ui4VU6I065f0w",
+      "live": false
+    });
+    
+    client.uploadCardImage(selectedFile)
+    .then((cardId) => {
+      console.log(cardId)
+      navigate("/carddetails", {state: {id: cardId}});
+    })
+    .catch((error) => {
+      console.error(error)
+    });
 	};
 
   return (
@@ -75,7 +101,22 @@ export default function CardScan() {
         </div>     
         :
         <div className="scan-button">
-          <button onClick={loadScanView}>Scan Insurance Card</button>
+          <button className="scan-view-button" onClick={loadScanView}>Scan Insurance Card</button>
+          <div className="file-upload">
+            <input type="file" name="file" onChange={changeHandler} />
+            {isSelected ? (
+              <div>
+                <p>Filename: {selectedFile.name}</p>
+                <p>Filetype: {selectedFile.type}</p>
+                <p>Size in bytes: {selectedFile.size}</p>
+              </div>
+            ) : (
+              <p>Select a file to show details</p>
+            )}
+            <div>
+              <button onClick={handleSubmission}>Submit</button>
+            </div>
+          </div>
         </div>
       }
     </div>
--- src/pages/CardDetails.js
+++ src/pages/CardDetails.js
@@ -3,27 +3,62 @@ import { useLocation } from 'react-router-dom';
 
 export default function CardScan() {
   const {state: cardDetails} = useLocation();
-  const location = useLocation();
+  const [ card, setCard ] = useState({});
+
+  useEffect(() => {
+    const getCardDetails = async (cardId) => {
+      try {
+        const response = await fetch(`http://localhost:3000/cards/${cardId}`, {
+          method: 'GET',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        })
+        
+        const data = await response.json()
+        if (data.card.state == "completed") {
+          const {member_number, payer_name, rx_bin, rx_pcn, rx_group, member_name, plan_id, card_specific_id, client_name} = data.card.details;
+  
+          for (const [key, value] of Object.entries(data.card.details)){              
+            typeof value === 'object' ? cardDetails[key] = value.value : cardDetails[key] = value
+          }
+          setCard({...cardDetails})
+        } else {
+          getCardDetails(cardDetails.id)
+        }
+      } catch (err) {
+        console.error(err)
+      }
+    }
+
+    if (cardDetails.id) {
+      console.log(cardDetails.id)
+      getCardDetails(cardDetails.id)
+    } else {
+      setCard(cardDetails)
+    }
+  }, [])
   
   return (
     <div className="CardDetails">
-      {console.log("cardDetails: ", cardDetails)} 
+      {console.log("cardDetails: ", card)} 
       <header>~ Card Details ~</header>
       <table> 
         <thead>
-          <tr className="top"><th>Card Holder:</th><th>{cardDetails.memberName}</th></tr>
-          <tr><td>Member Number:</td><td>{cardDetails.memberNumber}</td></tr>
-          <tr><td>Insurance:</td><td>{cardDetails.payerName}</td></tr>
-          <tr><td>Plan ID:</td><td>{cardDetails.planId}</td></tr>
-          <tr><td>Plan name:</td><td>{cardDetails.planName}</td></tr>
-          <tr><td>RxBin:</td><td>{cardDetails.rxBin}</td></tr>
-          <tr><td>RxPCN:</td><td>{cardDetails.rxPCN}</td></tr>
-          <tr><td>RxGRP:</td><td>{cardDetails.rxGroup}</td></tr>
-          <tr><td>Dependents:</td><td>{cardDetails.dependentNames}</td></tr>
-          <tr><td>Start Date:</td><td>{cardDetails.startDate}</td></tr>
-          <tr><td>Card Specific ID:</td><td>{cardDetails.cardSpecificId}</td></tr>
-          <tr><td>Group Number:</td><td>{cardDetails.groupNumber}</td></tr>
-          <tr><td>Client Name:</td><td>{cardDetails.clientName}</td></tr>
+          <tr className="top"><th>Card Holder:</th><th>{card.memberName ? card.memberName : card['member_name']}</th></tr>
+          <tr><td>Member Number:</td><td>{card.memberNumber ? card.memberNumber : card['member_number']}</td></tr>
+          <tr><td>Insurance:</td><td>{card.payerName ? card.payerName : card['payer_name']}</td></tr>
+          <tr><td>Plan ID:</td><td>{card.planId ? card.planId : card['plan_id']}</td></tr>
+          <tr><td>Plan name:</td><td>{card.planName ? card.planName : card['plan_name']}</td></tr>
+          <tr><td>RxBin:</td><td>{card.rxBin ? card.rxBin : card['rx_bin']}</td></tr>
+          <tr><td>RxPCN:</td><td>{card.rxPCN ? card.rxPCN : card['rx_pcn']}</td></tr>
+          <tr><td>RxGRP:</td><td>{card.rxGroup ? card.rxGroup : card['rx_group']}</td></tr>
+          <tr><td>Dependents:</td><td>{card.dependentNames ? card.dependentNames : card['dependent_names']}</td></tr>
+          <tr><td>Start Date:</td><td>{card.startDate ? card.startDate : card['start_date']}</td></tr>
+          <tr><td>Card Specific ID:</td><td>{card.cardSpecificId ? card.cardSpecificId : card['card_specific_id']}</td></tr>
+          <tr><td>Group Number:</td><td>{card.groupNumber ? card.groupNumber : card['group_number']}</td></tr>
+          <tr><td>Client Name:</td><td>{card.clientName ? card.clientName : card['client_name']}</td></tr>
         </thead>
       </table>
     </div>
