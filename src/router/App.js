dcecb92f3b40189d2266bfb5c5a841382b9a2393
Cat <cathy.moran@teladochealth.com>
2022-03-27 17:30:01 -0500

    add profile page route

--- src/router/App.js
+++ src/router/App.js
@@ -4,6 +4,7 @@ import Nav from '../components/Nav'
 import Home from '../pages/Home'
 import LogIn from '../pages/LogIn'
 import CardDetails from '../pages/CardDetails'
+import Profile from '../pages/Profile'
 
 export const App = (props) => {
   return (
@@ -14,6 +15,7 @@ export const App = (props) => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LogIn />} />
         <Route path="/carddetails" element={<CardDetails />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   </>);
